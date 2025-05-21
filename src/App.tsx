f9d304c28ba5490a376191732b999df35c8532f4
Shobande James Oluwaseun <shobandejames@gmail.com>
2025-05-21 12:58:02 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
     <>
     {/* <Navbar /> */}
     <DarkGradient /> 
-    {/* <PurpleGridContainer /> */}
+    <div className=' bg-gradient-to-l from-purple-700 to-indigo-950 p-3 text-white mt-7 md:mt-0 h-12'>
+                        <p>Learn more </p>
+                    </div>
     </>
   )
 }
