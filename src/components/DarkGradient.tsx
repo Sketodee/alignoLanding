3e0c9c2776b9efe391fc19e92275a0c273969292
Shobande James Oluwaseun <shobandejames@gmail.com>
2025-05-21 13:04:49 +0100

    blur fix

--- src/components/DarkGradient.tsx
+++ src/components/DarkGradient.tsx
@@ -8,17 +8,27 @@ const DarkGradient = () => {
      <div className="relative w-full h-[844px] md:h-[1080px] lg:h-[800px] bg-black overflow-hidden ">
       <div className="relative w-full h-[844px] md:h-[1080px] lg:h-[800px] bg-gray-950">
         {/* Top left purple gradient - 40% width */}
-        <div className="
-        absolute aspect-square rounded-full bg-gradient-to-br from-purple-400 to-transparent opacity-40 blur-3xl
+        {/* <div className="
+        absolute  bg-gradient-to-br from-purple-400 to-transparent opacity-40 blur-3xl
         xl:-top-50 xl:-left-20 xl:w-2/5 xl:h-3/4
         lg:-top-20 lg:-left-20 lg:w-1/2 lg:h-3/4
         md:-top-30 md:-left-40 md:w-2/3
         -top-10 -left-20 w-2/3 h-1/2
-        "></div>
+        "></div> */}
+
+        <div className="
+  absolute bg-gradient-to-br from-purple-400 to-transparent opacity-40 [filter:blur(64px)] transform-gpu
+  xl:-top-50 xl:-left-20 xl:w-2/5 xl:h-3/4
+  lg:-top-20 lg:-left-20 lg:w-1/2 lg:h-3/4
+  md:-top-30 md:-left-40 md:w-2/3
+  -top-10 -left-20 w-2/3 h-1/2
+  z-0
+"></div>
+
         
         {/* Bottom right purple gradient - 40% width */}
         <div className="
-        absolute aspect-square rounded-full bg-gradient-to-tl from-purple-400 to-transparent opacity-40 blur-3xl
+        absolute  bg-gradient-to-tl from-purple-400 to-transparent opacity-40 blur-3xl
         xl:-bottom-50 xl:-right-20 xl:w-2/5 xl:h-3/4
         lg:-bottom-20 lg:-right-20 lg:w-1/2 lg:h-3/4
         md:-bottom-30 md:-right-20 md:w-2/3  
